39e434e5b027a65708f250e88e7328bfb6f68732
JoyBoyCr7 <105557080+JoyBoyCr7@users.noreply.github.com>
2023-08-21 18:45:15 -0500

    Update product.js

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,17 +1,32 @@
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData, Form } from 'react-router-dom'
 
-const Products = () => {
+const Product = () => {
     const product = useLoaderData()
-    console.log(product) 
+    console.log("here_")
+    console.log(product)
+    console.log("here")
+    const id =  product._id
+    console.log(id)
 
         return (
+            
             <div className='farm'>
+                
                 <h1>{product.productname}</h1>
-                <h2>{product.image}</h2>
-                <img src={product.image} alt={product.productname} />
                 <h5>{product.description}</h5>
+
+                <h1>Edit a Product</h1>
+
+            <Form action={`/update/:${id}`} method='PUT'>
+                <input type='text' name='productname' placeholder='Product Name'/>
+                <input type='text' name='image' placeholder='Product Image'/>
+                <input type='number' name='price' placeholder='Product Price'/>
+                <input type='text' name='description' placeholder='Product Description'/>
+                <input type='submit' value='Edit Product'/>
+            </Form>
             </div>
+            
         )
 }
 
-export default Products
+export default Product

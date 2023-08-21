7eff85a8007fde6c3d3b7441389627a1c70642b5
JoyBoyCr7 <105557080+JoyBoyCr7@users.noreply.github.com>
2023-08-21 18:09:42 -0500

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,19 +1,17 @@
 import { createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom'
 import App from './App'
 import Farms from './pages/farm'
-import Products from './pages/product'
+import Product from './pages/product'
 import Home from './pages/home'
 import { farmsLoader, farmLoader, productsLoader, productLoader } from './loaders'
 import { createFarmAction, updateFarmAction, deleteFarmAction, createProductAction, updateProductAction, deleteProductAction } from './actions'
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<App/>}>
-        <Route path='' element={<Home/>} loader={[farmsLoader, productsLoader, productLoader, farmLoader]}/>
-        <Route path='farm/:id' element={<Farms/>} loader={farmLoader}/>
-        <Route path='product/:id' element={<Products/>} loader={productLoader}/>
-        <Route path='create' action={[createFarmAction, createProductAction]}/>
-        <Route path='update/:id' action={[updateFarmAction, updateProductAction]}/>
-        <Route path='delete/:id' action={[deleteFarmAction, deleteProductAction]}/>
+        <Route path='' element={<Home/>} loader={productsLoader}/>
+        <Route path=':id' element={<Product/>} loader={productLoader}/>
+        <Route path='create' action={createProductAction} />
+        <Route path="update/:id" action={updateProductAction} /> 
     </Route>
 ))
 

604652f9876cd7a1b9604179baee88a208e161ce
JoyBoyCr7 <105557080+JoyBoyCr7@users.noreply.github.com>
2023-08-21 18:09:15 -0500

    Update loaders.js

--- src/loaders.js
+++ src/loaders.js
@@ -1,21 +1,23 @@
 import { api } from './api'
 
 
-export const farmsLoader = async () => {
-    const response = await fetch(`${api}/farm`)
-    const farms = await response.json()
-    return farms
-}
+// export const farmsLoader = async () => {
+//     const response = await fetch(`${api}/farm`)
+//     const farms = await response.json()
+//     return farms
+// }
 
-export const farmLoader = async ({params}) => {
-    const id = params.id
-    const response = await fetch(`${api}/farm/${id}`)
-    const farm = await response.json()
-    return farm
-}
+// export const farmLoader = async ({params}) => {
+//     const id = params.id
+//     const response = await fetch(`${api}/farm/${id}`)
+//     const farm = await response.json()
+//     return farm
+// }
 export const productsLoader = async () => {
     const response = await fetch(`${api}/product`)
+    console.log(response)
     const products = await response.json()
+    console.log(products)
     return products
 }
 

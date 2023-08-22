862dff799a991b4f2b5fbe05d786c120f835df6e
Mark D. Lalich <mdlalich@gmail.com>
2023-08-21 17:53:02 -0700

    the product b doneish...

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,19 @@
-import { Link, useLoaderData } from 'react-router-dom';
-
+import { Link, useLoaderData, Form } from 'react-router-dom';
 const Home = () => {
-    const products = useLoaderData(); 
-    
+    const products = useLoaderData();
     return (
         <div>
+<h1>Create a Product</h1>
+        <Form action='/create' method='POST'>
+            <input type='text' name='productname' placeholder='Product Name'/>
+            <input type='text' name='image' placeholder='Product Image'/>
+            <input type='number' name='price' placeholder='Product Price'/>
+            <input type='text' name='description' placeholder='Product Description'/>
+        <input type='submit' value='Create Product'/>
+        </Form>
             {products.map((product, index) => (
                 <div key={product._id} className='productname'>
-                    <Link to={`/product/${product._id}`}>
+                    <Link to={`/${product._id}`}>
                         <h1>{product.productname}</h1>
                     </Link>
                     <img src={product.image} alt={product.name} />
@@ -18,5 +24,4 @@ const Home = () => {
         </div>
     );
 };
-
 export default Home;

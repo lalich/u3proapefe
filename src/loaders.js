6cf6a4a38fca0be5fb38cea74c02f9638fee6acf
Mark D. Lalich <mdlalich@gmail.com>
2023-08-25 12:14:20 -0700

    try these choco chips

--- src/loaders.js
+++ src/loaders.js
@@ -3,43 +3,43 @@ import { redirect } from 'react-router-dom'
 
 // farmer auth check
 const authFCheck = () => {
-    const loggedIn = localStorage.getItem('loggedIn')
+    // const loggedIn = localStorage.getItem('loggedIn')
 
-    if(!loggedIn){
-        return false
-    }
-    return true
-    // const tokenCookie = document.cookie
-    // .split('; ')
-    // .find(row => row.startsWith('token'))
-    // console.log(tokenCookie)
-    // if (tokenCookie) {
-    //     const token = tokenCookie.split('=')[1]
-    //     return token !== undefined && token !== ''
-    // } else {
+    // if(!loggedIn){
     //     return false
-       
     // }
+    // return true
+    const tokenCookie = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('token'))
+    console.log(tokenCookie)
+    if (tokenCookie) {
+        const token = tokenCookie.split('=')[1]
+        return token !== undefined && token !== ''
+    } else {
+        return false
+       
+    }
 
 }
 // user auth chek
 const authUCheck = () => {
-    const loggedIn = localStorage.getItem('loggedIn')
+    // const loggedIn = localStorage.getItem('loggedIn')
 
-    if(!loggedIn){
-        return false
-    }
-    return true
-    // const tokenCookie = document.cookie
-    // .split('; ')
-    // .find(row => row.startsWith('token'))
-    
-    // if (tokenCookie) {
-    //     const token = tokenCookie.split('=')[1]
-    //     return token !== undefined && token !== ''
-    // } else {
+    // if(!loggedIn){
     //     return false
     // }
+    // return true
+    const tokenCookie = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('token'))
+    
+    if (tokenCookie) {
+        const token = tokenCookie.split('=')[1]
+        return token !== undefined && token !== ''
+    } else {
+        return false
+    }
 }
 // all home loader
 export const farmsAndProductsLoader = async () => {
